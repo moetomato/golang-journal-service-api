63b8a624052762a0f4c458f5b23d196f750942a1
moe tomato <moetomato@GPX1T0039P.(none)>
2025-01-20 10:53:13 +0900

    add :sparkles: router

---
+++ api/router.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/moetomato/golang-journal-service-api/controllers"
+	"github.com/moetomato/golang-journal-service-api/services"
+)
+
+func NewRouter(db *sql.DB) *mux.Router {
+	ser := services.NewAppService(db)
+	jcon := controllers.NewJournalController(ser)
+	ccon := controllers.NewCommentController(ser)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/journal", jcon.PostJournalHandler).Methods(http.MethodPost)
+	r.HandleFunc("/journal/list", jcon.JournalListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/journal/{id:[0-9]+}", jcon.JournalDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/journal/nice", jcon.PostNiceHandler).Methods(http.MethodPost)
+
+	r.HandleFunc("/comment", ccon.PostCommentHandler).Methods(http.MethodPost)
+
+	return r
+}
